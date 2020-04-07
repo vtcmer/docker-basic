fd4d2edb52b44a0c06413c3d99257e37395eeac0
Victor <vtcmer@gmail.com>
2020-04-07 16:40:50 +0200

    Añadir a mongo geoposición  de las cameras

--- test3/open-data/app-db/src/main/java/com/ztt/opendata/db/app/entity/CameraEntity.java
+++ test3/open-data/app-db/src/main/java/com/ztt/opendata/db/app/entity/CameraEntity.java
@@ -1,8 +1,11 @@
 package com.ztt.opendata.db.app.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.geo.Point;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Document(collection = "camera")
@@ -11,6 +14,8 @@ public class CameraEntity {
     @Id
     private String id;
     private Date date;
+    @GeoSpatialIndexed
+    private Point location;
 
     public String getId() {
         return id;
@@ -26,5 +31,13 @@ public class CameraEntity {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point location) {
+        this.location = location;
     }
 }
--- test3/open-data/app-db/src/main/java/com/ztt/opendata/db/app/repository/impl/ImageMongoRepository.java
+++ test3/open-data/app-db/src/main/java/com/ztt/opendata/db/app/repository/impl/ImageMongoRepository.java
@@ -4,8 +4,11 @@ import com.ztt.opendata.db.app.entity.CameraEntity;
 import com.ztt.opendata.db.app.model.Photo;
 import com.ztt.opendata.db.app.repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
 
 @Repository("imageMongoRepository")
 public class ImageMongoRepository implements ImageRepository {
@@ -20,6 +23,7 @@ public class ImageMongoRepository implements ImageRepository {
         CameraEntity cameraEntity = new CameraEntity();
         cameraEntity.setId(photo.getName());
         cameraEntity.setDate(photo.getDate());
+        cameraEntity.setLocation(new Point(photo.getLongitude().doubleValue(), photo.getLatitude().doubleValue()));
         this.mongoTemplate.save(cameraEntity);
     }
 }
